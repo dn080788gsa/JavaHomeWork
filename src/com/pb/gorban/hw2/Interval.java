667e7e0037cd81eb49e7c9d766677486a541ddf4
dn080788gsa <gas1988@gmail.com>
2021-10-12 20:40:03 +0300

    hw2 fixed

--- src/com/pb/gorban/hw2/Interval.java
+++ src/com/pb/gorban/hw2/Interval.java
@@ -6,14 +6,16 @@ public class Interval {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите значение от 0 до 100");
         int clientInt = scan.nextInt();
-        if(clientInt >=0 & clientInt<15) {
+        if(clientInt >=0 & clientInt<=14) {
             System.out.println("Введенное значение находится в промежутке [0 -14]");
-        } else if(clientInt >=15 & clientInt<36) {
+        } else if(clientInt >=15 & clientInt<=35) {
             System.out.println("Введенное значение находится в промежутке [15 - 35]");
-        } else if(clientInt >=36 & clientInt<51) {
+        } else if(clientInt >=36 & clientInt<=50) {
+            System.out.println("Введенное значение находится в промежутке [36 - 50]");
+        } else if(clientInt >=51 & clientInt<=100) {
             System.out.println("Введенное значение находится в промежутке [36 - 50]");
         } else {
-            System.out.println("Введенное значение находится в промежутке [51 - 100]");
+            System.out.println("Введенное значение находится за пределами промежутков");
         }
     }
 }
