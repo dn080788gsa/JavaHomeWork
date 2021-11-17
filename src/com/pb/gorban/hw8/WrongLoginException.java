b6f87c9140d64a4d991254378d77fd6c3ef50d55
dn080788gsa <gas1988@gmail.com>
2021-11-17 23:19:28 +0200

    hw8 fix

--- src/com/pb/gorban/hw8/WrongLoginException.java
+++ src/com/pb/gorban/hw8/WrongLoginException.java
@@ -1,6 +1,9 @@
 package com.pb.gorban.hw8;
 
 public class WrongLoginException extends Exception {
+    public WrongLoginException() {
+        super();
+    }
     public WrongLoginException(String msg) {
         super(msg);
     }
--- src/com/pb/gorban/hw8/WrongPasswordException.java
+++ src/com/pb/gorban/hw8/WrongPasswordException.java
@@ -1,6 +1,9 @@
 package com.pb.gorban.hw8;
 
 public class WrongPasswordException extends Exception {
+    public WrongPasswordException() {
+        super();
+    }
     public WrongPasswordException(String msg) {
         super(msg);
     }
