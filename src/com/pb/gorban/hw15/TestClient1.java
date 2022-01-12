915f489632b32058ba160c8050c7460819b47285
dn080788gsa <gas1988@gmail.com>
2022-01-12 19:04:14 +0200

    hw15 first chat

---
+++ src/com/pb/gorban/hw15/TestClient1.java
@@ -0,0 +1,7 @@
+package com.pb.gorban.hw15;
+
+public class TestClient1 {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+    }
+}
---
+++ src/com/pb/gorban/hw15/TestClient2.java
@@ -0,0 +1,7 @@
+package com.pb.gorban.hw15;
+
+public class TestClient2 {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+    }
+}
